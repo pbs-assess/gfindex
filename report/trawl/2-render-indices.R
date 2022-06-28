408d99c76a3b53fa618bc25a013c34b275e97191
Sean Anderson <sean@seananderson.ca>
2022-06-28 15:59:52 -0700

    Updates to sdmTMB fits

--- report/trawl/2-render-indices.R
+++ report/trawl/2-render-indices.R
@@ -1,51 +1,55 @@
-# update or call a previous version of sdmTMB
-# remotes::install_github("pbs-assess/sdmTMB", ref = "delta")
 library(dplyr)
 
-make_dat <- function(r, s) {
+make_dat <- function(r, s, .c) {
   expand.grid(
     species = s,
     region = r,
+    covariate_type = .c,
     stringsAsFactors = FALSE
   )
 }
 
 list_regions <- c(
-      "SYN QCS"
-    , "SYN WCVI"
-    , "SYN HS"
-    , "SYN WCHG"
-    )
+  "SYN QCS",
+  "SYN WCVI",
+  "SYN HS",
+  "SYN WCHG"
+)
 
 list_species <- c(
   "Canary Rockfish"
 )
 
-to_fit <- make_dat(list_regions, list_species)
+list_covariate_type <- c("with depth", "no depth")
+
+to_fit <- make_dat(list_regions, list_species, list_covariate_type)
 
 # https://github.com/rstudio/rmarkdown/issues/1673
-render_separately <- function(...) callr::r(
-  function(...) rmarkdown::render(..., envir = globalenv()),
-  args = list(...), show = TRUE)
+render_separately <- function(...) {
+  callr::r(
+    function(...) rmarkdown::render(..., envir = globalenv()),
+    args = list(...), show = TRUE
+  )
+}
 
-fit_index <- function(region, species) {
+fit_index <- function(region, species, covariate_type) {
   spp <- gsub(" ", "-", gsub("\\/", "-", tolower(species)))
-  model_name <- "with depth" # describe model covariates
-  # model_name <- "no depth" # describe model covariates
   region_name <- region
   try({
     render_separately("report/trawl/index-standardization.Rmd",
       params = list(
         species = species,
         region = region,
-        name = model_name,
+        covariate_type = covariate_type,
         update_model = TRUE,
         update_index = TRUE,
         silent = TRUE
       ),
-      output_file = paste0(spp, "-",
-        gsub(" ", "-", gsub("\\/", "-", model_name)), "-",
-        gsub(" ", "-", gsub("\\/", "-", region_name)), ".html")
+      output_file = paste0(
+        spp, "-",
+        gsub(" ", "-", gsub("\\/", "-", covariate_type)), "-",
+        gsub(" ", "-", gsub("\\/", "-", region_name)), ".html"
+      )
     )
   })
 }
@@ -57,11 +61,10 @@ is_unix <- .Platform$OS.type == "unix"
 if (is_unix && !is_rstudio) {
   future::plan(future::multicore, workers = 6L)
 } else {
-  future::plan(future::multisession, workers = 2L)
+  future::plan(future::multisession, workers = 4L)
 }
 options(future.rng.onMisuse = "ignore")
 
 furrr::future_pwalk(to_fit, fit_index)
 
 future::plan(future::sequential)
-
