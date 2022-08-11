7b3a407b38f9f33bd96634d03804a60a5c59b287
Sean Anderson <sean@seananderson.ca>
2022-08-11 11:44:17 -0700

    Tidy stitching sim

--- stitch/stitch.R
+++ stitch/stitch.R
@@ -3,122 +3,197 @@ library(ggplot2)
 library(here)
 library(sdmTMB)
 
-region <- c("SYN QCS", "SYN HS", "SYN WCVI", "SYN WCHG")
-# region <- c("SYN WCVI")
-species <- "arrowtooth flounder"
+source("stitch/utils.R")
 
-dat <- readRDS(here("data/all-survey-sets-2021-select.rds")) %>%
-  dplyr::filter(species_common_name == tolower(species)) %>%
-  dplyr::filter(survey_abbrev %in% region)
-# change from per m2 to per km2:
-dat$density <- dat[["density_kgpm2"]] * 1000000
-dat$log_depth <- log(dat$depth_m)
-dat$area_swept1 <- dat$doorspread_m * (dat$speed_mpm * dat$duration_min)
-dat$area_swept2 <- dat$tow_length_m * dat$doorspread_m
-dat$area_swept <- ifelse(!is.na(dat$area_swept2), dat$area_swept2, dat$area_swept1)
-dat <- dplyr::filter(dat, !is.na(area_swept))
-dat <- sdmTMB::add_utm_columns(dat, c("longitude", "latitude"), utm_crs = 32609)
+dat <- prep_data()
+list_species <- read_species_list("stitch/spp.txt")
 
-formula <- catch_weight ~ s(log_depth, k = 5)
-# formula <- catch_weight ~ 0 + as.factor(year)
+out <- purrr::map(list_species, ~
+    fit_index(
+      dat,
+      species = .x
+    )
+) %>% setNames(list_species)
+index <- purrr::map_dfr(out, "index", .id = "species")
 
-set_priors <- sdmTMBpriors(
-  matern_s = pc_matern(range_gt = 10, sigma_lt = 4),
-  matern_st = pc_matern(range_gt = 10, sigma_lt = 3)
-)
+out_nodepth <- purrr::map(list_species, ~
+    fit_index(
+      dat,
+      species = .x,
+      formula = catch_weight ~ 1,
+    )
+) %>% setNames(list_species)
+index_nodepth <- purrr::map_dfr(out_nodepth, "index", .id = "species")
 
-# set_priors <- sdmTMBpriors(
-#   matern_s = pc_matern(range_gt = 5, sigma_lt = 3),
-#   matern_st = pc_matern(range_gt = 5, sigma_lt = 3)
+# out_ar1 <- purrr::map(list_species, ~
+#     fit_index(
+#       dat,
+#       species = .x,
+#       spatiotemporal = "ar1"
+#     )
+# ) %>% setNames(list_species)
+#
+# out_iid <- purrr::map(list_species, ~
+#     fit_index(
+#       dat,
+#       species = .x,
+#       formula = catch_weight ~ 0 + as.factor(year),
+#       spatiotemporal = "iid"
+#     )
+# ) %>% setNames(list_species)
+
+out_dg_nodepth <- purrr::map(list_species, ~
+    fit_index(
+      dat,
+      species = .x,
+      formula = catch_weight ~ 1,
+      family = delta_gamma()
+    )
+) %>% setNames(list_species)
+index_dg_nodepth <- purrr::map_dfr(out_dg_nodepth, "index", .id = "species")
+
+out_dg <- purrr::map(list_species, ~
+    fit_index(
+      dat,
+      species = .x,
+      family = delta_gamma()
+    )
+) %>% setNames(list_species)
+index_dg <- purrr::map_dfr(out_dg, "index", .id = "species")
+
+
+# out_pg <- purrr::map(list_species, ~
+#     fit_index(
+#       dat,
+#       formula = catch_weight ~ 1,
+#       species = .x,
+#       family = delta_poisson_link_gamma()
+#     )
+# ) %>% setNames(list_species)
+
+# out_dg <- purrr::map(list_species, ~
+#     fit_index(
+#       dat,
+#       species = .x,
+#       spatiotemporal = list("off", "rw"),
+#       family = delta_gamma()
+#     )
 # )
 
-spde <- make_mesh(dat, xy_cols = c("X", "Y"), cutoff = 20)
+index <- index %>% mutate(type = "Tweedie s(depth)")
+index_nodepth <- index_nodepth %>% mutate(type = "Tweedie")
+index_dg <- index_dg %>% mutate(type = "Delta-Gamma s(depth)")
+index_dg_nodepth <- index_dg_nodepth %>% mutate(type = "Delta-Gamma")
 
-# bnd <- INLA::inla.nonconvex.hull(cbind(dat$X, dat$Y), convex = -0.04)
-# inla_mesh <- INLA::inla.mesh.2d(
-#   boundary = bnd,
-#   max.edge = c(50, 120),
-#   offset = c(-0.01, -0.02),
-#   cutoff = c(20, 100),
-#   min.angle = c(21, 21)
-# )
-# mesh <- make_mesh(dat, c("X", "Y"), mesh = inla_mesh)
-# plot(mesh$mesh, asp = 1)
-# points(dat$X, dat$Y, pch = ".")
-# mesh$mesh$n
+mult <- 1000
 
-ctrl <- sdmTMBcontrol(newton_loops = 2L)
+g <- index %>%
+  bind_rows(index_nodepth) %>%
+  bind_rows(index_dg) %>%
+  bind_rows(index_dg_nodepth) %>%
+  group_by(species, type) %>%
+  mutate(max_log = max(log_est)) %>%
+  filter(max_log < 20) %>%
+  mutate(species = stringr::str_to_title(species)) %>%
+  ggplot(aes(year, est / mult, colour = type, fill = type,
+    ymin = lwr / mult, ymax = upr / mult)) +
+  ggsidekick::theme_sleek() +
+  facet_wrap(vars(species), scales = "free_y")
+g <- g +
+  # geom_pointrange(aes(ymin = lwr / mult, ymax = upr / mult))
+  geom_line() +
+  geom_ribbon(alpha = 0.2, colour = NA)
+g <- g + ylab("Biomass (tonnes)") +
+  # ggtitle(stringr::str_to_title(species), subtitle = paste(region, collapse = ", ")) +
+  coord_cartesian(
+    ylim = c(0, 70000),
+    expand = FALSE, xlim = range(index$year) + c(-0.25, 0.25)
+  ) +
+  theme(axis.title.x = element_blank()) +
+  facet_wrap(~type)
+g
 
-mesh <- make_mesh(dat, c("X", "Y"), cutoff = 20)
-plot(mesh$mesh, asp = 1)
-points(dat$X, dat$Y, pch = ".")
-mesh$mesh$n
+# AIC(out$`arrowtooth flounder`$fit)
+# AIC(out_nodepth$`arrowtooth flounder`$fit)
+# AIC(out_dg$`arrowtooth flounder`$fit)
+# AIC(out_dg_nodepth$`arrowtooth flounder`$fit)
 
-# yrs <- unique(dat$year)
-# all_yrs <- seq(min(dat$year), max(dat$year))
-# base::setdiff(yrs, all_yrs)
+ind <- index %>%
+  bind_rows(index_nodepth) %>%
+  bind_rows(index_dg) %>%
+  bind_rows(index_dg_nodepth)
 
-nd <- readRDS(here("grids/synoptic_grid.rds")) %>%
-  dplyr::filter(survey %in% region)
-make_grid <- function(.x, years) {
-  years <- sort(unique(years))
-  .nd <- do.call(
-    "rbind",
-    replicate(length(years), .x, simplify = FALSE)
-  )
-  .nd$year <- rep(years, each = nrow(.x))
-  .nd
-}
+group_by(ind, type) %>%
+  summarise(mean_se = mean(se))
+
+simulate(out$`arrowtooth flounder`$fit, 200) %>%
+  dharma_residuals(out$`arrowtooth flounder`$fit)
+
+simulate(out_dg$`arrowtooth flounder`$fit, 200) %>%
+  dharma_residuals(out$`arrowtooth flounder`$fit)
+
+simulate(out_dg_nodepth$`arrowtooth flounder`$fit, 200) %>%
+  dharma_residuals(out$`arrowtooth flounder`$fit)
+
+r0 <- residuals(out$`arrowtooth flounder`$fit, "mle-mcmc",
+  model = 1L, mcmc_iter = 101, mcmc_warmup = 100)
+
+r1 <- residuals(out_dg$`arrowtooth flounder`$fit, "mle-mcmc",
+model = 1L, mcmc_iter = 101, mcmc_warmup = 100)
+
+r2 <- residuals(out_dg$`arrowtooth flounder`$fit, "mle-mcmc",
+  model = 2L, mcmc_iter = 101, mcmc_warmup = 100)
+
+save(r0, r1, r2, file = "~/src/arrowtooth/arrowtooth-nongit/geostat-figs/mcmc-resids.rda")
+
+
+
+m <- out_dg$`arrowtooth flounder`$fit
+nd <- readRDS(here("grids/synoptic_grid.rds"))
+dat <- out_dg$`arrowtooth flounder`$fit$data
 fitted_yrs <- sort(unique(dat$year))
 nd <- make_grid(nd, years = fitted_yrs)
 nd <- na.omit(nd)
 nd$year <- as.integer(nd$year)
 nd$log_depth <- log(nd$depth)
-nd$cell_area <- 4e+6
+p <- predict(m, newdata = nd)
 
-TMB::openmp(n = 3, DLL = "sdmTMB")
-fit <- try(
-  sdmTMB(
-    formula,
-    dat,
-    mesh = spde,
-    time = "year",
-    family = delta_gamma(),
-    # family = tweedie(),
-    spatial = "on",
-    spatiotemporal = "rw",
-    offset = log(dat$area_swept / 100000),
-    share_range = TRUE,
-    anisotropy = FALSE,
-    silent = FALSE,
-    control = ctrl,
-    do_index = TRUE,
-    index_args = list(area = nd$cell_area / 100000),
-    predict_args = list(newdata = nd, return_tmb_object = TRUE),
-  )
+data <- dat
+coast <- gfplot:::load_coastline(
+  range(data$longitude) + c(-0.5, 0.5),
+  range(data$latitude) + c(-0.5, 0.5),
+  utm_zone = 9
 )
-
-s <- sanity(fit)
-fit
-
-# p <- predict(fit, newdata = nd, return_tmb_object = TRUE)
-# ind <- get_index(p, bias_correct = TRUE, area = nd$cell_area / 100000)
-ind <- get_index(fit, bias_correct = TRUE)
-
-mult <- 1000
-g <- ggplot(ind, aes(year, est / mult)) +
-  ggsidekick::theme_sleek()
-g <- g + geom_pointrange(aes(ymin = lwr / mult, ymax = upr / mult),
-  alpha = 1, colour = "grey20"
+coords <- coord_equal(
+  expand = FALSE, xlim = range(data$X) + c(-10, 10),
+  ylim = range(data$Y) + c(-10, 10)
 )
-g <- g + ylab("Biomass (tonnes)") +
-  ggtitle(stringr::str_to_title(species), subtitle = paste(region, collapse = ", ")) +
-  coord_cartesian(
-    ylim = c(0, max(ind$upr / mult) * 1.03),
-    expand = FALSE, xlim = range(ind$year) + c(-0.25, 0.25)
-  ) +
-  theme(axis.title.x = element_blank())
-g
+utm_labs <- labs(x = "Easting", y = "Northing")
+
+plot_map <- function(dat, column) {
+  ggplot(dat, aes_string("X", "Y", fill = column, colour = column)) +
+    geom_polygon(
+      data = coast, aes_string(x = "X", y = "Y", group = "PID"),
+      fill = "grey87", col = "grey70", lwd = 0.2, inherit.aes = FALSE
+    ) +
+    geom_tile(width = 2, height = 2) +
+    scale_colour_viridis_c(trans = "sqrt") +
+    scale_fill_viridis_c(trans = "sqrt") +
+    coord_fixed() +
+    coords +
+    utm_labs
+}
+
+p %>%
+  mutate(est_total = plogis(est1) * exp(est2)) %>%
+  mutate(est_total = ifelse(est_total > quantile(est_total, probs = 0.995), quantile(est_total, probs = 0.995), est_total)) %>%
+  # filter(year %in% 2007:2009) %>%
+  plot_map("est_total") +
+  facet_wrap(vars(year)) +
+  labs(fill = "Biomass density", colour = "Biomass density") +
+  gfplot::theme_pbs()
+
+ggsave("")
 
 # g <- ggplot(ind, aes(year, est / 1000)) +
 #   ggsidekick::theme_sleek()
