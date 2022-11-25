abe7010bc3f5814b15790cd5ce91593e7714699a
Sean Anderson <sean@seananderson.ca>
2022-11-24 18:29:09 -0800

    Develop sim experiment

---
+++ stitch/scenarios.R
@@ -0,0 +1,147 @@
+n_year <- 20L
+
+base <- list(
+  n_year = n_year,
+  label = "empty",
+  gap_size = 0.25,
+  sim_coefs = c(2, 5),
+  phi = 7,
+  obs_sampled_size = 200L, # or 200 if blocking first
+  obs_yrs = list(
+    north_yrs = seq(1, n_year - 1, 2), south_yrs = c(seq(2, n_year, 2))
+  ),
+  region_cutoff = 0.50,
+  range = 0.5,
+  sigma_O = 1.2,
+  sigma_E = 0.4,
+  year_arima.sim = list(ar = 0.6),
+  year_marginal_sd = 0.3
+)
+
+sc <- purrr::map(seq_len(20), ~base)
+
+i <- 1
+
+sc[[i]]$label <- "Base"
+sc[[i]]$category <- "Base"
+i <- i + 1
+
+# Gap
+
+sc[[i]]$label <- "No gap"
+sc[[i]]$category <- "Gap"
+sc[[i]]$gap_size <- 0
+i <- i + 1
+
+sc[[i]]$label <- "Large gap"
+sc[[i]]$category <- "Gap"
+sc[[i]]$gap_size <- 0.5
+i <- i + 1
+
+sc[[i]]$label <- "Small gap"
+sc[[i]]$category <- "Gap"
+sc[[i]]$gap_size <- 0.1
+i <- i + 1
+
+# Covariate
+
+sc[[i]]$label <- "No covariate"
+sc[[i]]$category <- "Abundance gradient"
+sc[[i]]$sim_coefs <- c(0, 0)
+i <- i + 1
+
+# Observation error
+
+sc[[i]]$label <- "Low observation error"
+sc[[i]]$category <- "Observation error"
+sc[[i]]$phi <- 3
+i <- i + 1
+
+sc[[i]]$label <- "High observation error"
+sc[[i]]$category <- "Observation error"
+sc[[i]]$phi <- 14
+i <- i + 1
+
+# Observation sample size
+
+sc[[i]]$label <- "Low sample size"
+sc[[i]]$category <- "Sample size"
+sc[[i]]$obs_sampled_size <- 100L # FIXME
+i <- i + 1
+
+sc[[i]]$label <- "High sample size"
+sc[[i]]$category <- "Sample size"
+sc[[i]]$obs_sampled_size <- 400L # FIXME
+i <- i + 1
+
+# Year of overlap
+
+sc[[i]]$label <- "Year of overlap"
+sc[[i]]$category <- "Year overlap"
+sc[[i]]$obs_yrs <- list(north_yrs = c(seq(1, n_year - 1, 2), n_year), south_yrs = c(seq(2, n_year, 2)))
+i <- i + 1
+
+# Unequal regions
+
+sc[[i]]$label <- "Unequal regions"
+sc[[i]]$category <- "Region size"
+sc[[i]]$region_cutoff <- 0.25
+i <- i + 1
+
+# Range size
+
+sc[[i]]$label <- "Low range"
+sc[[i]]$category <- "Range"
+sc[[i]]$range <- 0.25  # .25, .5, 1 cod nfld: 180/
+i <- i + 1
+
+sc[[i]]$label <- "High range"
+sc[[i]]$category <- "Range"
+sc[[i]]$range <- 1  # .25, .5, 1 cod nfld: 180/
+i <- i + 1
+
+# Marginal RF SD
+
+sc[[i]]$label <- "Low sigma O"
+sc[[i]]$category <- "Spatial SD"
+sc[[i]]$sigma_O <- 0.4
+i <- i + 1
+
+# Annual variability stuff N(0, SD)
+
+sc[[i]]$label <- "High year SD"
+sc[[i]]$category <- "Annual variability"
+sc[[i]]$year_marginal_sd <- 1.0
+i <- i + 1
+
+sc[[i]]$label <- "No year SD"
+sc[[i]]$category <- "Annual variability"
+sc[[i]]$year_marginal_sd <- 0.0001
+sc[[i]]$year_arima.sim <- list(ar = 0)
+i <- i + 1
+
+# Annual variability AR1 stuff
+
+sc[[i]]$label <- "Year AR(1) rho = 1"
+sc[[i]]$category <- "Annual correlation"
+sc[[i]]$year_arima.sim <- list(ar = 0.9999)
+i <- i + 1
+
+sc[[i]]$label <- "Year AR(1) rho = 0"
+sc[[i]]$category <- "Annual correlation"
+sc[[i]]$year_arima.sim <- list(ar = 0)
+i <- i + 1
+
+# Annual extremes
+
+sc[[i]]$label <- "Years black-swan like"
+sc[[i]]$category <- "Annual extremes"
+sc[[i]]$heavy_sd_mult <- 6
+sc[[i]]$heavy_sd_frac <- 0.1
+i <- i + 1
+
+sc[[i]]$label <- "Years heavy-tailed mixture"
+sc[[i]]$category <- "Annual extremes"
+sc[[i]]$heavy_sd_mult <- 3
+sc[[i]]$heavy_sd_frac <- 1/3
+i <- i + 1
--- stitch/sim-seesaw-repeated.R
+++ stitch/sim-seesaw-repeated.R
@@ -19,7 +19,6 @@
 # - RW deviations adapting to fit?
 # - fix depth covariates correctly - works?
 
-
 # quadratic, linear, or breakpoint covariate effect?
 
 library(sdmTMB)
@@ -29,373 +28,23 @@ source("stitch/funcs.R")
 dir.create("stitch/figs", showWarnings = FALSE)
 # Simulation testing survey stitching with various models -----------------
 
-# n_year <- 12 # even number
-# .seed <- 28817
-# .seed <- 288171
-
-# predictor_grid <- expand.grid(
-#   X = seq(0, 1, length.out = 100), Y = seq(0, 1, length.out = 100),
-#   year = seq_len(n_year)
-# )
-# mesh_sim <- make_mesh(predictor_grid, xy_cols = c("X", "Y"), cutoff = 0.1)
-
-# keep it linear?
-
-xx <- seq(0, 1, length.out = 100)
-plot(xx, xx * 4 + 10 * xx^2, ylab = "Depth effect", xlab = "Depth value")
-lines(xx, xx * 2 + 5 * xx^2, ylab = "Depth effect", xlab = "Depth value")
-
-# xx <- seq(0, 1, length.out = 100)
-# plot(xx, xx * 4 + 10 * xx^2, ylab = "Depth effect", xlab = "Depth value")
-# lines(xx, xx * 2 + 5 * xx^2, ylab = "Depth effect", xlab = "Depth value")
-
-
-sim_fit_and_index <- function(n_year,
-                              .seed,
-                              gap_size = 0.3,
-                              obs_sampled_size = 400L,
-                              year_marginal_sd = 0.3,
-                              obs_yrs = list(
-                                north_yrs = seq(1, n_year - 1, 2),
-                                south_yrs = seq(2, n_year, 2)
-                              ),
-                              phi = 8,
-                              region_cutoff = 0.5,
-                              range = 0.8,
-                              sigma_O = 1.6,
-                              year_arima.sim = list(ar = 0.6),
-                              sim_coefs = c(2, 5)) {
-  is_even <- function(x) x %% 2 == 0
-  if (!is_even(n_year)) cli::cli_abort("Number of years must be even.")
-
-  cli::cli_alert_info(glue::glue("Running simulation for seed ", .seed))
-
-  cli::cli_alert_success("Creating mesh...")
-  predictor_grid <- expand.grid(
-    X = seq(0, 1, length.out = 100), Y = seq(0, 1, length.out = 100),
-    year = seq_len(n_year)
-  )
-  mesh_sim <- make_mesh(predictor_grid, xy_cols = c("X", "Y"), cutoff = 0.1)
-
-  cli::cli_alert_success("Simulating...")
-  x <- sim(predictor_grid, mesh_sim,
-    seed = .seed, phi = phi, range = range,
-    region_cutoff = region_cutoff,
-    year_arima.sim = year_arima.sim, year_marginal_sd = year_marginal_sd,
-    coefs = sim_coefs,
-    north_effect = 0, sigma_O = sigma_O
-  )
-  sim_dat <- x$sim_dat
-  predictor_dat <- x$predictor_dat
-
-  # Visualize what we just did ----------------------------------------------
-
-  # # Year effects:
-  # ggplot(data.frame(x = seq_len(n_year), y = x$year_effects), aes(x, y)) +
-  #   geom_line()
-  # ggplot(data.frame(x = seq_len(n_year), y = x$year_effects), aes(x, exp(y))) +
-  #   geom_line()
-  #
-  # blank_theme_elements <- theme(panel.grid.major = element_line(colour = "grey90"),
-  #   # panel.spacing.x = unit(20, "pt"),
-  #   axis.text = element_blank(), axis.ticks = element_blank(),
-  #   axis.title = element_blank(), legend.position = "right")
-  #
-  # # Spatio-temporal truth
-  # ggplot(sim_dat, aes(X, Y, fill = eta)) +
-  #   geom_raster() +
-  #   facet_wrap(vars(year)) +
-  #   scale_fill_viridis_c() +
-  #   ggsidekick::theme_sleek() +
-  #   coord_equal(expand = FALSE) +
-  #   blank_theme_elements +
-  #   labs(fill = "True\nsimulated\nlog abundance")
-  # # ggsave("stitch/figs/spatio-temporal-truth.pdf", width = 8, height = 5)
-
-  # Sample N per year -----------------------------------------------------
-
-  cli::cli_alert_success("Observing...")
-  d <- observe(
-    sim_dat,
-    sample_n = obs_sampled_size,
-    region_cutoff = region_cutoff,
-    seed = .seed,
-    north_yrs = obs_yrs$north_yrs,
-    south_yrs = obs_yrs$south_yrs, gap = gap_size
-  )
-
-  # Visualize it ------------------------------------------------------------
-
-  # ggplot(d, aes(X, Y, colour = log(observed))) +
-  #   geom_point() +
-  #   facet_wrap(vars(year)) +
-  #   scale_colour_viridis_c() +
-  #   ggsidekick::theme_sleek() +
-  #   blank_theme_elements +
-  #   coord_equal(expand = FALSE) +
-  #   theme(panel.grid.major = element_line(colour = "grey90"))
-  # # panel.spacing.x = unit(20, "pt")
-  # # ggsave("stitch/figs/spatio-temporal-observed.pdf", width = 8, height = 5)
-
-  # Calculate known true biomass/abundance ----------------------------------
-
-  regions_samp <- select(d, year, sampled_region) %>% distinct()
-
-  actual <- group_by(sim_dat, year) %>%
-    summarise(total = sum(mu)) |>
-    left_join(regions_samp, by = "year")
-
-  # ggplot(actual, aes(year, total)) +
-  #   geom_line()
-
-  # Fit models --------------------------------------------------------------
-
-  cli::cli_alert_success("Fitting models...")
-  mesh <- make_mesh(d, c("X", "Y"), cutoff = 0.1)
-  priors <- sdmTMBpriors(
-    matern_s = pc_matern(range_gt = 0.3, sigma_lt = 0.4),
-    matern_st = pc_matern(range_gt = 0.3, sigma_lt = 0.3)
-  )
-
-  fits <- list()
-  nms <- c()
-  i <- 1
-
-  fits[[i]] <- sdmTMB(
-    observed ~ 1,
-    family = tweedie(),
-    data = d, time = "year", spatiotemporal = "rw", spatial = "on",
-    silent = TRUE, mesh = mesh,
-    priors = priors
-  )
-  nms <- c(nms, "RW")
-  i <- i + 1
-
-  fits[[i]] <- sdmTMB(
-    observed ~ 0 + as.factor(year) + depth_cov + I(depth_cov^2),
-    family = tweedie(),
-    data = d, time = "year", spatiotemporal = "iid", spatial = "on",
-    silent = TRUE, mesh = mesh,
-    priors = priors
-  )
-  nms <- c(nms, "IID covariate")
-  i <- i + 1
-
-  fits[[i]] <- sdmTMB(
-    observed ~ s(year),
-    family = tweedie(),
-    data = d, time = "year", spatiotemporal = "iid", spatial = "on",
-    silent = TRUE, mesh = mesh,
-    priors = priors
-  )
-  nms <- c(nms, "IID s(year)")
-  i <- i + 1
-
-  fits[[i]] <- sdmTMB(
-    observed ~ 0 + as.factor(year),
-    family = tweedie(),
-    data = d, time = "year", spatiotemporal = "iid", spatial = "on",
-    mesh = mesh,
-    priors = priors
-  )
-  i <- i + 1
-  nms <- c(nms, "IID")
-
-  fits[[i]] <- sdmTMB(
-    observed ~ 0,
-    family = tweedie(),
-    time_varying = ~1,
-    data = d, time = "year", spatiotemporal = "iid", spatial = "on",
-    mesh = mesh,
-    priors = priors
-  )
-  i <- i + 1
-  nms <- c(nms, "IID RW year")
-
-  fits[[i]] <- sdmTMB(
-    observed ~ 0,
-    family = tweedie(),
-    time_varying = ~1, time_varying_type = "ar1",
-    data = d, time = "year", spatiotemporal = "iid", spatial = "on",
-    mesh = mesh,
-    priors = priors
-  )
-  i <- i + 1
-  nms <- c(nms, "IID AR1 year")
-
-  fits[[i]] <- sdmTMB(
-    observed ~ 0 + as.factor(year),
-    family = tweedie(),
-    data = d, time = "year", spatiotemporal = "off", spatial = "on",
-    mesh = mesh,
-    priors = priors
-  )
-  i <- i + 1
-  nms <- c(nms, "Spatial only")
-
-  names(fits) <- nms
-
-  # Predict on grid and calculate indexes -----------------------------------
-
-  cli::cli_alert_success("Calculating indices...")
-  nd <- select(predictor_dat, X, Y, year, region)
-  nd$depth_cov <- nd$Y
-  preds <- purrr::map(fits, predict, newdata = nd, return_tmb_object = TRUE)
-  indexes <- purrr::map(preds, get_index, bias_correct = TRUE)
-
-  indexes_df <- dplyr::bind_rows(indexes, .id = "model") |>
-    mutate(with_depth = paste0("covariate = ", grepl("covariate", model))) |>
-    mutate(type = gsub(" covariate", "", model))
-
-  indexes_df <- left_join(indexes_df, actual, by = "year") |>
-    mutate(seed = .seed)
-  indexes_df
-}
-
-# Plot it -----------------------------------------------------------------
-
-# out <- purrr::map_dfr(seq_len(1L), ~ sim_fit_and_index(n_year = 12L, .seed = .x))
-# out <- purrr::map_dfr(seq_len(10L), ~ sim_fit_and_index(n_year = 12L, .seed = .x))
-#
-
-# study design:
-# - [ ] no gap, small gap, large gap `gap_size`
-#       gap_size = 0.00
-#       gap_size = 0.25
-#       gap_size = 0.50
-
-# - [ ] no covariate effect; strong covariate effect
-#       sim_coefs = c(2, 5)
-#       sim_coefs = c(0, 0)
-
-# - [ ] lower or increasing observation error ('phi')
-#       phi = 2
-#       phi = 7
-
-# - [ ] try lowering or increasing numbers of samples
-#       obs_sampled_size = 200L
-#       obs_sampled_size = 400L
-
-# - [ ] one year of complete coverage at beginning or end or none TODO
-#       obs_yrs = list(north_yrs = seq(1, n_year - 1, 2), south_yrs = c(seq(2, n_year, 2))
-#       obs_yrs = list(north_yrs = c(seq(1, n_year - 1, 2), n_year), south_yrs = c(seq(2, n_year, 2))
-
-# - [ ] impact of uneven north south vs. even?
-#       region_cutoff = 0.50
-#       region_cutoff = 0.25
-
-n_year <- 12L
-
-base <- list(
-  n_year = n_year,
-  label = "empty",
-  gap_size = 0.25,
-  sim_coefs = c(2, 5),
-  phi = 8,
-  obs_sampled_size = 400L, # or 200 if blocking first
-  obs_yrs = list(
-    north_yrs = seq(1, n_year - 1, 2), south_yrs = c(seq(2, n_year, 2)),
-    region_cutoff = 0.50,
-    range = 0.8,
-    sigma_O = 1.6,
-    year_marginal_sd = 0.2
-  )
-)
-
-sc <- purrr::map(seq_len(14), ~base)
-
-i <- 1
-
-sc[[i]]$label <- "Base"
-i <- i + 1
-
-sc[[i]]$label <- "No gap"
-sc[[i]]$gap_size <- 0
-i <- i + 1
-
-sc[[i]]$label <- "Large gap"
-sc[[i]]$gap_size <- 0.5
-i <- i + 1
-
-sc[[i]]$label <- "No covariate"
-sc[[i]]$sim_coefs <- c(0, 0)
-i <- i + 1
-
-sc[[i]]$label <- "Low observation error"
-sc[[i]]$phi <- 3
-i <- i + 1
-
-sc[[i]]$label <- "High observation error"
-sc[[i]]$phi <- 14
-i <- i + 1
-
-sc[[i]]$label <- "Low sample size"
-sc[[i]]$obs_sampled_size <- 200L # FIXME
-i <- i + 1
-
-sc[[i]]$label <- "High sample size"
-sc[[i]]$obs_sampled_size <- 800L # FIXME
-i <- i + 1
-
-sc[[i]]$label <- "Year of overlap"
-sc[[i]]$obs_yrs <- list(north_yrs = c(seq(1, n_year - 1, 2), n_year), south_yrs = c(seq(2, n_year, 2)))
-i <- i + 1
-
-sc[[i]]$label <- "Unequal regions"
-sc[[i]]$region_cutoff <- 0.25
-i <- i + 1
-
-sc[[i]]$label <- "Low range"
-sc[[i]]$range <- 0.2  # .25, .5, 1 cod nfld: 180/
-i <- i + 1
-
-sc[[i]]$label <- "Low sigma O"
-sc[[i]]$sigma_O <- 0.2
-i <- i + 1
-
-sc[[i]]$label <- "High year SD"
-sc[[i]]$year_marginal_sd <- 0.8
-i <- i + 1
-
-sc[[i]]$label <- "Low year SD"
-sc[[i]]$year_marginal_sd <- 0.05
-i <- i + 1
-
-# SD
-# FIXME: highly variable and 0.8
-# student-t... with low AR1 and high SD
-# normal(0, 0.00001)
-# normal(0, 0.2)
-# normal(0, 0.4)
-
-# (with AR1)
-# heavy tailedness
-# regular (normal())
-# heavy normal(0, base) + normal(0, base * 2) 25% of time?
-# very heavy normal(0, base) + normal(0, base * 4) 35% of time?
-
-#
-# AR1 = 0.5
-# AR1 = 0
-# AR1 = 1
-
-# no gradient done
-# base done
-# high gradient north to south
-
-if (any(grepl("empty", purrr::map_chr(sc, "label")))) {
-  stop("Too many slots")
-}
-
-names(sc) <- purrr::map_chr(sc, "label")
+source(here::here("stitch/scenarios.R"))
+if (any(grepl("empty", purrr::map_chr(sc, "label")))) stop("Too many slots")
+labels <- unname(purrr::map_chr(sc, "label"))
+names(sc) <- labels
+categories <- unname(purrr::map_chr(sc, "category"))
+lu <- data.frame(label = labels, category = categories, stringsAsFactors = FALSE)
 sc <- purrr::map(sc, ~ {
   .x$label <- NULL
+  .x$category <- NULL
   .x
 })
 
-# out <- purrr::pmap_dfr(sc, sim_fit_and_index, .id = "label")
+# tictoc::tic()
+# out20 <- do.call(sim_fit_and_index, c(sc[[1]], .seed = 1, make_plots = F))
+# tictoc::toc()
 
-seeds <- seq_len(20L)
+seeds <- seq_len(2L)
 out_df <- purrr:::map_dfr(seeds, function(seed_i) {
   x <- list()
   for (i in seq_along(sc)) {
@@ -405,11 +54,11 @@ out_df <- purrr:::map_dfr(seeds, function(seed_i) {
   names(x) <- names(sc)
   bind_rows(x, .id = "label")
 })
-saveRDS(out_df, "stitch/sawtooth-sim-nov4.rds")
+out_df2 <- left_join(out_df, lu, by = "label")
+saveRDS(out_df2, "stitch/sawtooth-sim-nov4.rds")
 
 # ---------------------------------------------
 # iterate above ....
-
 
 # out_df$label <- forcats::fct_inorder(out_df$label)
 
