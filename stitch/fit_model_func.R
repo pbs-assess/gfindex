7b6c3256db07c53648cbeee142cd14282c87d72e
Jillian Dunic <>
2023-05-08 11:17:09 -0700

    include extra_time for models that can use it

--- stitch/fit_model_func.R
+++ stitch/fit_model_func.R
@@ -9,18 +9,24 @@ check_sanity <- function(x) {
 
 fit_models <- function(
     dat, catch, data_subset = NULL, mesh = NULL, cutoff = 20, family = tweedie(), 
-    offset = NULL, 
+    offset = NULL, use_extra_time = TRUE,
     ctrl = sdmTMBcontrol(nlminb_loops = 1L, newton_loops = 1L)) {
 
   if (is.null(data_subset)) {
     data_subset <- unique(dat$species_common_name)
   }
 
-  message(cat("\tFitting models for data subset:", data_subset, "\n"))
+  message(cat("\n\tFitting models for data subset:", data_subset, "\n"))
 
   if (is.null(mesh)) {
-    message(cat("\n\tNo mesh provided, making mesh with cutoff:", cutoff, "\n"))
+    message(cat("\t- No mesh provided, making mesh with cutoff:", cutoff))
     mesh <- make_mesh(dat, c("X", "Y"), cutoff = 20)
+  }
+
+  extra_time <- NULL
+  if (use_extra_time) {
+    extra_time <- sdmTMB:::find_missing_time(dat$year)
+    message(cat("\t- Filling in extra_time with:", extra_time, "\n"))
   }
 
   dat <- droplevels(dat)  # drop extra factor levels before running models
@@ -35,6 +41,7 @@ fit_models <- function(
       data = dat, time = "year", spatiotemporal = "rw", spatial = "on",
       silent = TRUE, mesh = mesh,
       offset = offset,
+      extra_time = extra_time,
       control = ctrl
     )
   )
@@ -96,6 +103,7 @@ fit_models <- function(
       data = dat, time = "year", spatiotemporal = "iid", spatial = "on",
       mesh = mesh,
       offset = offset,
+      extra_time = extra_time,
       control = ctrl
     )
   )
@@ -156,21 +164,24 @@ fit_models <- function(
 # }
 
 # -------
-get_pred_list <- function(fit_list, newdata) 
+get_pred_list <- function(fit_list, newdata, newdata_extra_time = NULL) {
   fit_list %>% 
   purrr::map(., function(.x) {
+    newdata <- newdata
     if (inherits(.x, "sdmTMB")) {
+      if(!is.null(.x$extra_time)) newdata <- newdata_extra_time
       out <- predict(.x, newdata = newdata, return_tmb_object = TRUE)
     } else {
       out <- NA
     }
     out
   })
+}
 
-get_index_list <- function(pred_list, area = 1) {
+get_index_list <- function(pred_list) {
   purrr::map(pred_list, function(.x) {
     if (length(.x) > 1) {
-      get_index(.x, bias_correct = TRUE, area = area)
+      get_index(.x, bias_correct = TRUE, area = .x$data$area)
     } else {
       out <- NA  # keep empty fits as visual cue that these did not fit when plotting
     }
